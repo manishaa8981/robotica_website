1fc6200d7801c5185d888b18a372b4279f5551ef
manishaa8981 <manishaa8981@gmail.com>
2025-12-17 12:42:09 +0545

    nextjs congif

--- next.config.mjs
+++ next.config.mjs
@@ -14,7 +14,7 @@ const nextConfig = {
       },
       {
         protocol: "https",
-        hostname: "api.internationalroboticscollege.com",
+        hostname: "api.roboticainstitute.com",
         pathname: "/uploads/**",
       },
     ],
